ce3e1603e8a98b6eebff39dd0e20de672790f0cb
Chris Johnston <chris@thejohnstons.net>
2021-12-11 01:42:46 -0800

    add patterns

--- snow/snow.ino
+++ snow/snow.ino
@@ -1,144 +1,206 @@
 #include <avr/io.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
 
 #define PIN_1 0 // row 1
 #define PIN_2 1 // row 2
 #define PIN_3 2 // row 3
 #define PIN_4 3 // col 1
-#define PIN_5 4 // col 2
+#define PIN_BUTTON 4 // col 2
+
+#define DEBOUNCE_TIME 80
+#define NUM_PATTERNS 8
+
+// track idle time
+volatile unsigned long wake_time = 0;
+volatile unsigned long button_debounce = 0;
+
+// 0 - sleeping
+// 1 - random blinking
+volatile int pattern = 1;
+
+ISR(PCINT0_vect)
+{
+    // this applies when awake
+    if (pattern != 0 && digitalRead(PIN_BUTTON))
+    {
+        // only update pattern if less than debounce time
+        auto time = millis();
+        if ((time - button_debounce) > DEBOUNCE_TIME)
+        {
+            pattern += 1;
+            pattern %= NUM_PATTERNS;
+
+            button_debounce = millis();
+        }
+    }
+}
 
 void setup()
 {
+    power_all_disable(); // only enable what we need
+    power_timer0_enable();
+
+    // pin change interrupts
+    GIMSK = (1 << PCIE);
+    PCMSK |= (1 << PCINT4);
+
     pinMode(PIN_1, OUTPUT);
     pinMode(PIN_2, OUTPUT);
     pinMode(PIN_3, OUTPUT);
     pinMode(PIN_4, OUTPUT);
-    pinMode(PIN_5, INPUT_PULLUP);
+    pinMode(PIN_BUTTON, INPUT_PULLUP);
 
-    while (1) {
-        
-        if (!digitalRead(PIN_5))
-        {
-            for (int i = 0; i < 6; i++)
-            {
-                turnOffLeds();
-                turnOnLed(i);
-                delay(50);
-            }
-            turnOffLeds();
-            delay(5);
-            // for (int i = 1; i < 6; i++)
-            // {
-            //     turnOffLeds();
-            //     turnOnLed(5 - i);
-            //     delay(300);
-            // }
-        }
-        else
-        {
-            turnOnLed(random(6));
-            delay(5);
-            turnOnLed(random(6));
-            delay(5);
-            turnOffLeds();
-            delay(300 + random(300));
+    turnOffLeds();
+    delay(50); // prevent LEDs being on messing with programming
 
-        }
-        
-
-    // 0b0010 - 1
-    // 0b1101 - 2
-    // 0b0100
-    // 0b1011 - 4
-    // 0b1000
-    // 0b0111
-    // for (int i = 0; i < 6; i++)
-    // {
-    //     turnOnLed(i);
-
-    //     delayMicroseconds(5);
-
-    //     turnOffLeds();
-
-    //     delay(35);
-    // }
+    button_debounce = millis();
     
-    // digitalWrite(PIN_1, 0);
-    // digitalWrite(PIN_2, 0);
-    // digitalWrite(PIN_3, 1);
-    // digitalWrite(PIN_4, 1); // col 
-    // digitalWrite(PIN_5, 0); // col 
+    animateWakeup();
+}
 
-    // delay(1000);
-
-    // digitalWrite(PIN_1, LOW);
-    // digitalWrite(PIN_2, LOW);
-    // digitalWrite(PIN_3, LOW);
-    // digitalWrite(PIN_4, LOW);
-    // digitalWrite(PIN_5, LOW);
-
-    // delay(1000);
-
+void animateWakeup()
+{
+    for (auto i = 0; i < 6; i++)
+    {
+        turnOffLeds();
+        turnOnLed(i);
+        delay(50);
     }
+
+    turnOffLeds();
+    delay(100);
+}
+
+void animateShutoff()
+{
+    for (auto i = 0; i < 6; i++)
+    {
+        turnOffLeds();
+        turnOnLed(6 - i);
+        delay(50);
+    }
+
+    turnOffLeds();
+    delay(100);
 }
 
 void loop()
 {
-    for (auto i = 0; i < 8; i++)
+    switch (pattern)
     {
-        // 4 and 5 do not work
-        for (auto k = 1; k < i + 1; k++)
-        {
-            turnOnLed(i);
-            delay(50);
+        case 1:
+            pwm();
+            // turnOnLed(0);
+            // delay(5);
+            // turnOffLeds();
+            //delay(250);
+            return;
+        case 2:
+            randomBlink();
+            return;
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+            turnOnLed(pattern - 2);
+            return;
+        case 0:
+        default:
+            sleep();
 
-            turnOffLeds();
-            delay(250);
-        }
+            animateWakeup();
+            // unset flag on wake
+            pattern = 1;
+            return;
+    }
+}
 
-        turnOffLeds();
-        delay(1000);
+void randomBlink()
+{
+    // turn on 1..3 leds each time
+    for (auto i = 1; i < 4; i++)
+    {
+        turnOnLed(random(6));
+        delay(3);
     }
 
-    // turnOnLed(3);
-    // delay(1000);
+    turnOffLeds();
 
-    // turnOnLed(4);
-    // delay(1000);
+    delay(300 + random(300));
+}
 
-    // turnOffLeds();
+void softPwmFade(int period, int iters, int set, bool inverse, int biasOn, int biasOff)
+{
+    for (auto i = 0; i < period; i++)
+    {
+        auto y = inverse ? period - i : i;
+        y += biasOn;
+        auto z = inverse ? i : period - i;
+        z += biasOff;
+        for (auto d = 0; d < iters; d++)
+        {
+            // PORTB = set;
+            overlayPORTB(set);
+            delayMicroseconds(y);
 
-    // delay(3000);
+            turnOffLeds();
+            delayMicroseconds(z);
+        }
+    }
+}
+
+void pwm()
+{
+    // turn on odd LEDs
+    auto odd = 0b0001;
+    auto even = 0b1110;
+    auto period = 250;
+    auto iters = 15;
+    auto biasOn = 0;
+    auto biasOff = 100;
+
+    softPwmFade(period, iters, odd, false, biasOn, biasOff);
+    softPwmFade(period, iters, odd, true, biasOn, biasOff);
+    turnOffLeds();
+
+    delay(100);
+
+    softPwmFade(period, iters, even, false, biasOn, biasOff);
+    softPwmFade(period, iters, even, true, biasOn, biasOff);
+    turnOffLeds();
+
+    delay(100);
+
+}
+
+void sleep()
+{
+    animateShutoff();
+
+    turnOffLeds();
+
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    power_all_disable();
+    sleep_cpu();
+
+    // on wake
+    power_timer0_enable();
+    wake_time = millis();
 }
 
 void turnOnLed(int idx)
 {
-    // 1 | 2 | 3 | 4 |  LED
-    // --------------------
-    // 1   0   0   0    0
-    // 0   1   0   0    2
-    // 0   0   1   0    4
-    // 0   0   0   1    6
-
-    // 0   1   1   1    1
-    // 1   0   1   1    3
-    // 1   1   0   1    5
-    // 1   1   1   0    7
-
-    int lut[] = {
+   int lut[] = {
         0b1101,
         0b0010,
         0b1011,
         0b0100,
         0b0111,
         0b1000,
-        // 0b1000,
-        // 0b0100,
-        // 0b0010,
-        // 0b0001,
-        // 0b0111,
-        // 0b1011,
-        // 0b1101,
-        // 0b1110,
     };
 
     auto v = lut[idx] & 0b1111;
@@ -146,34 +208,16 @@ void turnOnLed(int idx)
     auto existing = PORTB & 0b10000;
 
     PORTB = existing | v;
+}
 
-    // digitalWrite(PIN_1, (v & 0b1) > 0);
-    // digitalWrite(PIN_2, (v & 0b10) > 0);
-    // digitalWrite(PIN_3, (v & 0b100) > 0);
-    // digitalWrite(PIN_4, (v & 0b1000) > 0);
+void overlayPORTB(int value)
+{
+    auto existing = PORTB & 0b10000;
 
-    // auto high = HIGH;
-    // auto low = LOW;
-
-    // if (idx % 2 == 0)
-    // {
-    //     high = LOW;
-    //     low = HIGH;
-    // }
-
-    // for (auto i = 1; i < 5; i++)
-    // {
-    //     digitalWrite(i, low);
-    // }
-    // digitalWrite((idx / 2) + 1, high);
+    PORTB = existing | value;
 }
 
 void turnOffLeds()
 {
-    // digitalWrite(PIN_1, LOW);
-    // digitalWrite(PIN_2, LOW);
-    // digitalWrite(PIN_3, LOW);
-    // digitalWrite(PIN_4, LOW);
-
     PORTB &= 0b10000;
 }
